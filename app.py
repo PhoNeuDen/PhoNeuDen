17d73fa56b7f8478b31ac22a74091a62ee74cdb9
PHONEUDEN <157373040+PhoNeuDen@users.noreply.github.com>
2024-01-24 16:14:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -676,9 +676,19 @@ with tab4:
                       color = z_curve, range_x=[scale_x_left, scale_x_right], range_y = [scale_y_bottom, scale_y_upper],
                       color_continuous_scale=px.colors.sequential.Jet)
         st.plotly_chart(fig)
-        fig, axes = plt.subplots(figsize=(10,10), sharex=True, sharey=True)
+     #exporting as pdf
+        pdf = FPDF()
+        pdf.add_page()
+        with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
+          fig.savefig(tmpfile.name)
+          pdf.image(tmpfile.name, 10, 10, (plot_w*16), (plot_h*16))
+        st.download_button(
+          "Download 2D Cross Plot as PDF",
+          data=pdf.output(dest='S').encode('latin-1'),
+          file_name=f"{well_name}_2D_Cross_Plot.pdf",
+        )
 
-        ax1 = plt.subplot2grid((1,1), (0,0), rowspan=2, colspan = 2)
+        
         lsX = np.linspace(0,0.45,46)
         ssCnlX = np.empty((np.size(lsX),0), float)
         dolCnlX = np.empty((np.size(lsX),0), float)
@@ -694,6 +704,7 @@ with tab4:
         x = las_df['NPHI']
         y = las_df['RHOB']
         cbar=las_df['DEPT']
+        ax1 = plt.subplot2grid((1,1), (0,0), rowspan=2, colspan = 2)
         ax1.scatter(x,y,c='r', alpha=0.5)
         ax1.set_title("ND Crossplot")
         ax1.set_xlabel("Neutron Porosity [v.v]")
